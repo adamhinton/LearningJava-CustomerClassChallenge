83160345c10c83242089879241bc7016619069ae
adamd <adam.d.hinton@gmail.com>
2024-02-20 21:24:21 -0500

    three overload constructors

---
+++ src/Customer.java
@@ -0,0 +1,31 @@
+public class Customer {
+    private String name;
+    private int creditLimit;
+    private String email;
+
+    public Customer(String name, int creditLimit, String email) {
+        this.name = name;
+        this.creditLimit = creditLimit;
+        this.email = email;
+    }
+
+    public Customer() {
+        this("defaultName", 1000, "default@email.com");
+    }
+
+    public Customer(String name, String email) {
+        this(name, 1000, email);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCreditLimit() {
+        return creditLimit;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
